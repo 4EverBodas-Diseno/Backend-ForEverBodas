7512e9b86aed9cddb166c7c93072792c5566cf62
danieln3m0 <u202219315@upc.edu.pe>
2024-09-20 15:33:58 -0500

    Refactor app.js to add CORS and remove unused code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // src/app.js
 const express = require('express');
+const cors = require('cors'); // Importa CORS
 const userRoutes = require('./entities/user/user.routes');
-//const productRoutes = require('./entities/product/product.routes');
 const profileRoutes = require('./entities/profile/profile.routes'); // Agregado
 const wellwishRoutes = require('./entities/wellWish/wellwish.routes'); // Agregado
 const guestRoutes = require('./entities/guest/guest.routes'); // Agregado
@@ -11,19 +11,16 @@ const webpageRoutes = require('./entities/webpage/webpage.routes'); // Agregado
 const summaryRoutes = require('./entities/summary/summary.routes'); // Agregado
 const notificationRoutes = require('./entities/notification/notification.routes'); // Agregado
 
-
 const { swaggerUi, specs } = require('./swagger');
-
-
 
 const app = express();
 
 // Middlewares
+app.use(cors()); // Configura CORS para permitir solicitudes desde cualquier origen
 app.use(express.json());
 
 // Rutas
 app.use('/api/users', userRoutes);
-//app.use('/api/products', productRoutes);
 app.use('/api/profiles', profileRoutes); // Agregado
 app.use('/api/wellwishes', wellwishRoutes); // Agregado
 app.use('/api/guests', guestRoutes); // Agregado
@@ -33,8 +30,6 @@ app.use('/api/webpages', webpageRoutes); // Agregado
 app.use('/api/summaries', summaryRoutes); // Agregado
 app.use('/api/notifications', notificationRoutes); // Agregado
 
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-
 
 module.exports = app;
