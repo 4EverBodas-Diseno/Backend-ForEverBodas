87149386a31268484873fc43e484a60a38b87661
danieln3m0 <u202219315@upc.edu.pe>
2024-09-24 06:22:54 -0500

    Refactor guest model to include totalInvitados and totalConfirmados virtual properties

--- src/entities/guest/guest.model.js
+++ src/entities/guest/guest.model.js
@@ -1,16 +1,33 @@
 // src/entities/guest/guest.model.js
 const mongoose = require('mongoose');
 
+// Definir el esquema de invitado
 const guestSchema = new mongoose.Schema({
   GuestID: { type: String, required: true, unique: true },
   WebPageID: { type: mongoose.Schema.Types.ObjectId, ref: 'WebPage', required: true },
   UserID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   Nombre: { type: String, required: true },
   Correo: { type: String, required: true },
-  EstadoInvitacion: { type: String, enum: ['Invitado', 'Confirmado', 'Rechazado'], default: 'Invitado' },
+  EstadoInvitacion: { type: String, enum: ['I', 'C', 'R'], default: 'Invitado' },
   Telefono: { type: String },
-  URL: { type: String }
+  URL: { type: String },
+  Confirmado: { type: Boolean, default: false },
+  numMaxAcompanantes: { type: Number, default: 0 }, 
+  numAcompanantes: { type: Number, default: 0 }, 
 });
+
+
+guestSchema.virtual('totalInvitados').get(function () {
+  return this.model('Guest').countDocuments();
+});
+
+
+guestSchema.virtual('totalConfirmados').get(function () {
+  return this.model('Guest').countDocuments({ Confirmado: true });
+});
+
+
+guestSchema.set('toJSON', { virtuals: true });
 
 const Guest = mongoose.model('Guest', guestSchema);
 
