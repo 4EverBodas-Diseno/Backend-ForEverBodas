e83b7f5a67391703aa95a6e3b167db4cc7091cca
danieln3m0 <u202219315@upc.edu.pe>
2024-09-24 16:42:54 -0500

    Refactor app.js to include color routes

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@
 const express = require('express');
 const cors = require('cors'); // Importa CORS
 const userRoutes = require('./entities/user/user.routes');
-const loginRoutes = require('./entities/login/login.routes'); // Agregado
 const profileRoutes = require('./entities/profile/profile.routes'); // Agregado
 const wellwishRoutes = require('./entities/wellWish/wellwish.routes'); // Agregado
+const colorRoutes = require('./entities/color/color.routes'); // Agregado
 const guestRoutes = require('./entities/guest/guest.routes'); // Agregado
 const weddingRoutes = require('./entities/wedding/wedding.routes'); // Agregado
 const webpageRoutes = require('./entities/webpage/webpage.routes'); // Agregado
@@ -21,13 +21,13 @@ app.use(express.json());
 
 // Rutas
 app.use('/api/users', userRoutes);
+app.use('/api/colors', colorRoutes);
 app.use('/api/profiles', profileRoutes); // Agregado
 app.use('/api/wellwishes', wellwishRoutes); // Agregado
 app.use('/api/guests', guestRoutes); // Agregado
 app.use('/api/weddings', weddingRoutes); // Agregado
 app.use('/api/webpages', webpageRoutes); // Agregado
 app.use('/api/notifications', notificationRoutes); // Agregado
-app.use('/api/login', loginRoutes); // Agregado
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
