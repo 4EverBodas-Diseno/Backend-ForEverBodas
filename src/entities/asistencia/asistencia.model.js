03097c3c557a4b4caa8c7149d80632863e06f6b5
danieln3m0 <u202219315@upc.edu.pe>
2024-09-24 16:43:00 -0500

    Refactor asistencia model to use string references instead of object IDs

--- src/entities/asistencia/asistencia.model.js
+++ src/entities/asistencia/asistencia.model.js
@@ -2,8 +2,8 @@
 const mongoose = require('mongoose');
 
 const asistenciaSchema = new mongoose.Schema({
-  GuestID: { type: mongoose.Schema.Types.ObjectId, ref: 'Guest', required: true },
-  EventID: { type: mongoose.Schema.Types.ObjectId, ref: 'Wedding', required: true },
+  GuestID: { type: String, ref: 'Guest', required: true },
+  EventID: { type: String, ref: 'Wedding', required: true },
   Confirmacion: { type: Boolean, required: true },
   Cant_Invitado: { type: Number, required: true }
 });
